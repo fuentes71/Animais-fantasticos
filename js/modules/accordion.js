1e1e1bd75e31a79e47750b9dfb462d81789253dc
Matheus Fuentes <matheus.fu.p@hotmail.com>
2022-02-16 17:38:16 -0300

    atualização

---
+++ js/modules/accordion.js
@@ -0,0 +1,19 @@
+
+const activeClass = 'ativo'
+export default function initAccordion() {
+  
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+  if(accordionList.length) {
+
+    accordionList[0].classList.add(activeClass)
+    accordionList[0].nextElementSibling.classList.add(activeClass)
+    function activeAccordion() {
+      this.classList.toggle('ativo');
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    })
+  };
+};
