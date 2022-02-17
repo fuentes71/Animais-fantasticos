7467b8dcfeca4d54ccc5e4663d3a167620aab40e
Matheus Fuentes <matheus.fu.p@hotmail.com>
2022-02-16 23:50:18 -0300

    test

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import initScrtrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 
-
+const teste = {}
 initTabNav()
 initAnimacaoScroll();
 initScrtrollSuave();
