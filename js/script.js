4182ea810712c8d6736d691546a04dda1dbed770
Matheus Fuentes <matheus.fu.p@hotmail.com>
2022-02-17 12:32:48 -0300

    tooltip

--- js/script.js
+++ js/script.js
@@ -2,9 +2,13 @@ import initTabNav from './modules/initTab.js';
 import initScrtrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/Tooltip.js';
 
-const teste = {}
+
+initModal()
 initTabNav()
 initAnimacaoScroll();
 initScrtrollSuave();
 initAccordion();
+initTooltip()
